c5a4a75682fe4a1fe7bb7d5a9e19317089cef4f8
ewandjeeric <ewandjeeric@gmail.com>
2022-05-13 09:09:15 +0100

    ..

---
+++ src/main/java/org/ged/CloudExterieurApplication.java
@@ -0,0 +1,13 @@
+package org.ged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudExterieurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudExterieurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ged/CloudExterieurApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudExterieurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
